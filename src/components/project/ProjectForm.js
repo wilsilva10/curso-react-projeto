c6ba1026a42b7e7c41705e0cde22bf31cfbb8448
Wilber Silva <wilberoliveira1@hotmail.com>
2023-05-08 18:28:35 -0300

    Form dinamico

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,20 +1,25 @@
-function ProjectForm() {
+import styles from "./ProjectForm.module.css";
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+
+function ProjectForm({btnText}) {
   return (
-    <form>
-      <div>
-        <input type="text" placeholder="Insira o nome do projeto" />
-      </div>
-      <div>
-        <input type="number" placeholder="Insira o orçamento projeto" />
-      </div>
-      <div>
-        <select name="category_id">
-          <option disabled selected>Selecione a categoria</option>
-        </select>
-      </div>
-      <div>
-      <input type="submit" value="Criar projeto" />
-      </div>
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Nome do projeto"
+        name="name"
+        placeholder="Insira o nome do projeto"
+      />
+      <Input
+        type="number"
+        text="Orçamento Proejto"
+        name="budget"
+        placeholder="Insira o orçamento total"
+      />
+      <Select name="category_id" text="Selecione a categoria" />
+      <SubmitButton text={btnText}/>
     </form>
   );
 }
