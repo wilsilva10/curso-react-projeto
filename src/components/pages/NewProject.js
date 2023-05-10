f0e3945c57f2b5cef022eff5e4fd6c716184fcbb
Wilber Silva <wilberoliveira1@hotmail.com>
2023-05-10 15:24:31 -0300

    atualizando modal

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,30 +1,28 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 
-import ProjectForm from "../project/ProjectForm";
-import styles from "./NewProject.module.css";
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
 
 function NewProject() {
-  const navigate = useNavigate();
+  const navigate = useNavigate ()
 
   function createPost(project) {
-    // iniciando cost and services
-    project.costs = 0;
-    project.services = [];
+    // initialize cost and services
+    project.cost = 0
+    project.services = []
 
-    fetch("http://localhost:5000/projects", {
-      method: "POST",
+    fetch('http://localhost:5000/projects', {
+      method: 'POST',
       headers: {
-        "Content-type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(project),
     })
-      .then((resp) => resp.json)
+      .then((resp) => resp.json())
       .then((data) => {
-        console.log(data);
-        //redirect
-        navigate("/projects", { message: "Projeto criado com sucesso!" });
+        navigate('/projects', {state:{ message: 'Projeto criado com sucesso!' }})
       })
-      .catch((err) => console.log(err));
   }
 
   return (
@@ -33,7 +31,7 @@ function NewProject() {
       <p>Crie seu projeto para depois adicionar os serviços</p>
       <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
-  );
+  )
 }
 
-export default NewProject;
+export default NewProject
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,27 @@
-function Project() {
-  return <h1>Todos os Projetos</h1>
+
+import { useLocation } from 'react-router-dom'
+
+
+
+
+import Message from '../layout/Message'
+
+
+function Projects() {
+  const location = useLocation()
+  let message = ''
+  if (location.state) {
+    message = location.state.message
+  }
+
+
+
+  return (
+    <div>
+      <h1>Meus projetos</h1>
+      {message && <Message type="success" msg={message} />}
+    </div>
+  )
 }
 
-export default Project;
+export default Projects
