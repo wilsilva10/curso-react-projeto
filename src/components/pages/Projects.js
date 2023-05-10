1f35a01e7dd66b236f551ce740fcd6ea56b6f81f
Wilber Silva <wilberoliveira1@hotmail.com>
2023-05-10 15:46:41 -0300

    atualizando projects pag

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,27 +1,30 @@
+import { useLocation } from "react-router-dom";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
-import { useLocation } from 'react-router-dom'
+import styles from "./Projects.module.css";
 
-
-
-
-import Message from '../layout/Message'
-
+import Message from "../layout/Message";
 
 function Projects() {
-  const location = useLocation()
-  let message = ''
+  const location = useLocation();
+  let message = "";
   if (location.state) {
-    message = location.state.message
+    message = location.state.message;
   }
 
-
-
   return (
-    <div>
-      <h1>Meus projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto"/>
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
